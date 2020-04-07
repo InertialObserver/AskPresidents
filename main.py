de25b30c2dddd0cabe3e00d5643685dda481f608
Jessi Iler <inertial.observer@gmail.com>
2020-04-07 03:54:18 -0700

    Adjust text box size

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-import random
+
 
 class AskPresidentsLayout(BoxLayout):
         def choice(self, guess):
             output = guess
-            if self.ids.result.text == "":
+            if self.ids.result.text == "":  # if the result box is empty, then print the result. If there is already a result, do nothing.
                 self.ids.result.text = output
+
+        def clear(self):
+            self.ids.result.text = ""  # erases the results so user can guess again
 
 class AskPresidents(App):
     def build(self):
